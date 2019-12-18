2ef197f947432c5e37c7cc55e9d3145167e5578d
DF-happy <1392691393@qq.com>
2019-12-18 12:06:02 +0800

    5.16  <我的接单>（张会纳 霍东芳）

--- app/src/main/java/com/wulianwang/lsp/activity/MainActivity.java
+++ app/src/main/java/com/wulianwang/lsp/activity/MainActivity.java
@@ -2,15 +2,27 @@ package com.wulianwang.lsp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.wulianwang.lsp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.textView);
+        textView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this,FirstActivity.class);
+        startActivity(intent);
+
     }
 }
