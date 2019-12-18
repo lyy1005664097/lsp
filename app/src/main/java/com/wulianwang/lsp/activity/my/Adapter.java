314052b8976978611f9c30e32d354b11cefefa5a
李珍珍 <1973163532@qq.com>
2019-12-18 15:55:32 +0800

    李珍珍 康子露 5.12.2 第二次提交

--- app/src/main/java/com/wulianwang/lsp/activity/my/Adapter.java
+++ app/src/main/java/com/wulianwang/lsp/activity/my/Adapter.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import com.wulianwang.lsp.R;
+
 import java.util.LinkedList;
 
 public class Adapter extends BaseAdapter {
