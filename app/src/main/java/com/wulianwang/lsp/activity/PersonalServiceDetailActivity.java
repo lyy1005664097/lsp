fe5b3f5c71a1ebbda33b993b8f81d212968af2d2
dyh-hub <1340767721@qq.com>
2019-12-20 14:34:46 +0800

    杜一航 魏来 3.3   图标置换 跳转优化

--- app/src/main/java/com/wulianwang/lsp/activity/PersonalServiceDetailActivity.java
+++ app/src/main/java/com/wulianwang/lsp/activity/PersonalServiceDetailActivity.java
@@ -1,64 +1,60 @@
 package com.wulianwang.lsp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
+import android.widget.ImageView;
 import com.wulianwang.lsp.R;
 
 /**
  * 杜一航，魏来 3.3
  */
 public class PersonalServiceDetailActivity extends AppCompatActivity {
-    private TextView myTextView;
-    private Button bt1,bt2;
-    private EditText et1,et2,et3,et4,et5;
-
+    private ImageView a1, a2, a3;
+    private Button bb;
     boolean flag = false;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_service_detail);
-        myTextView = (TextView)this.findViewById(R.id.textView2);
-        et1 = (EditText)this.findViewById(R.id.editText);
-        bt1=(Button) this.findViewById(R.id.button2);
-        bt2=(Button)this.findViewById(R.id.button);
-        et2 = (EditText)findViewById(R.id.editText2);
-        et3 = (EditText)findViewById(R.id.editText3);
-        et4=(EditText)findViewById(R.id.editText4);
-        et5=(EditText)findViewById(R.id.editText5);
 
+        a1 = (ImageView) this.findViewById(R.id.imageView4);
+        a2 = (ImageView) this.findViewById(R.id.imageView7);
+        a3 = (ImageView) this.findViewById(R.id.imageView11);
+        bb = (Button)this.findViewById(R.id.button3);
+        if (flag) {
+            bb.setVisibility(View.VISIBLE);
+        } else {
+            bb.setVisibility(View.GONE);
 
-        if(flag){
-            bt1.setVisibility(View.VISIBLE);
-        }else{
-            bt1.setVisibility(View.GONE);
-
-            bt1.setOnClickListener(new View.OnClickListener() {
+            a1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     //点击事件
-            //        Intent i=new Intent(PersonalServiceDetailActivity.this,PPAP.class);
-           //         startActivity(i);
-
-     bt2.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-        //     Intent x=new Intent(PersonalServiceDetailActivity.this,PPP.class);
-       //      startActivity(x);
-         }
-     });
+                    Intent i = new Intent(PersonalServiceDetailActivity.this, PersonalServiceActivity.class);
+                    startActivity(i);
 
                 }
             });
+            a2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent x = new Intent(PersonalServiceDetailActivity.this, MapSearchActivity.class);
+                    startActivity(x);
+                }
 
+            });
+            a3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent y = new Intent(PersonalServiceDetailActivity.this, PeopleListActivity.class);
+                    startActivity(y);
+
+                }
+            });
         }
     }
-        }
-
+};
