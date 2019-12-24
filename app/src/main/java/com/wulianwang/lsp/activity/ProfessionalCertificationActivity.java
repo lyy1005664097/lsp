dbb3dfb7392789a881df0d4043d4ec367095fa41
lyy1005664097 <1005664097@qq.com>
2019-12-24 16:37:42 +0800

    优化 ProfessionalCertificationActivity 的界面

--- app/src/main/java/com/wulianwang/lsp/activity/ProfessionalCertificationActivity.java
+++ app/src/main/java/com/wulianwang/lsp/activity/ProfessionalCertificationActivity.java
@@ -15,33 +15,40 @@ package com.wulianwang.lsp.activity;
 /**
  * 刘泽钜，杨瑞华 5.10
  */
-public class ProfessionalCertificationActivity extends AppCompatActivity {
+public class ProfessionalCertificationActivity extends BaseActivity {
+
+    Spinner sp;
+    Button btn;
+    String[] str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professional_certification);
-        Spinner spinner;
-        Button btn;
-        ImageView imv;
-        Spinner sp = (Spinner)this.findViewById(R.id.spinner);
-        imv=(ImageView)this.findViewById(R.id.imageView4);
+
+        initData();
+        initView();
+        setActionBar(true, "职业认证");
+    }
+
+    @Override
+    public void initData() {
+        str = new String[]{"请选择职业类型", "水电工", "水工", "外卖员", "建筑工"};
+    }
+
+    @Override
+    public void initView() {
+        sp = (Spinner)this.findViewById(R.id.spinner);
         btn=(Button)this.findViewById(R.id.button2);
-        String[] str = {"请选择职业类型", "水电工", "水工", "外卖员", "建筑工"};
+
         int String;
         ArrayAdapter<String> ad = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, str);
         sp.setAdapter(ad);
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ProfessionalCertificationActivity.this, MyInfoActivity.class);
-                startActivity(intent);}
-        });
-        imv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent =new Intent(ProfessionalCertificationActivity.this, MyInfoActivity.class);
-                startActivity(intent);
+                //    Intent intent = new Intent(ProfessionalCertificationActivity.this, MyInfoActivity.class);
+                //    startActivity(intent);
             }
         });
     }
